0fbd375ddc33a84d51f6993fd00a2d578fca5e9f
Tyler <b4rn3scode@gmail.com>
2020-06-29 09:27:18 -0400

    a

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 //$sessdir = dirname(dirname(__FILE__)).'/session';
 //ini_set('session.save_path', $sessdir);
 
